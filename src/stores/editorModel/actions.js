97197684dc23167a0c37ccc1484918b25c9d631d
George Czabania <george.czabania@sparkventures.co.nz>
2015-12-12 20:36:15 +1300

    Save to couchdb

--- src/stores/editorModel/actions.js
+++ src/stores/editorModel/actions.js
@@ -1,6 +1,13 @@
 import * as constants from './constants'
+import db from '../db'
 
 export function saveContent (editorContentModel) {
+  db.get('settext1').then((doc) => db.put({
+    _id: editorContentModel.name,
+    _rev: doc.rev,
+    content: editorContentModel.content,
+  }))
+
   return {
     type: constants.SAVE_CONTENT,
     payload: editorContentModel,
